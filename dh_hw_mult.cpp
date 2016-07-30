1dceac6434a4b2cc0de27d042d9758519b6758e3
nick2383 <nicholas.birch2@gmail.com>
2016-07-29 20:02:28 -0700

    add FSM to hardware multiplier

--- dh_hw_mult.cpp
+++ dh_hw_mult.cpp
@@ -2,6 +2,116 @@
 #include "digit.h"
 #include "dh_hw_mult.h"
 
+//*************************************************
+// state register
+void dh_hw_mult::state_reg() {
+  while(1) {
+    // reset the state machine
+    if (reset.read() == SC_LOGIC_1) {
+      state.write(S0_WAIT);
+    }
+    //
+    else {
+      state.write(next_state.read());
+    }
+    wait();
+  }
+}
+
+//*************************************************
+// state transition control
+void dh_hw_mult::state_transition() {
+
+  // default: self-loop
+  next_state.write(state.read());
+  
+  switch (state.read()) {
+
+    case S0_WAIT: 
+        if(hw_mult_enable.read() == true) {
+          next_state.write(S1_EXECUTE);
+        }
+        break;
+  
+    case S1_EXECUTE:
+        next_state.write(S2_OUTPUT);
+        break;
+
+    case S2_OUTPUT:
+        next_state.write(S3_FINISH);
+        break;
+
+    case S3_FINISH:
+        if(hw_mult_enable.read() == false) {
+          next_state.write(S0_WAIT);
+        }
+        break;
+  
+    default:
+        break;
+  }    
+}
+
+//*************************************************
+// state output control
+void dh_hw_mult::state_output() {
+
+  NN_DIGIT a[2], b, c, t, u;
+  NN_HALF_DIGIT bHigh, bLow, cHigh, cLow;
+
+  // defaults
+  out_data_low.write(out_data_low.read());
+  out_data_high.write(out_data_high.read());
+  hw_mult_done.write(false);
+  
+  switch (state.read()) {
+
+    case S0_WAIT: 
+        hw_mult_done.write(false);
+        break;
+  
+    case S1_EXECUTE:
+        // Read inputs  
+        b = in_data_1.read();
+        c = in_data_2.read();
+          
+        // Original code from NN_DigitMult()...   
+        bHigh = (NN_HALF_DIGIT)HIGH_HALF (b);
+        bLow = (NN_HALF_DIGIT)LOW_HALF (b);
+        cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
+        cLow = (NN_HALF_DIGIT)LOW_HALF (c);
+
+        a[0] = (NN_DIGIT)bLow * (NN_DIGIT)cLow;
+        t = (NN_DIGIT)bLow * (NN_DIGIT)cHigh;
+        u = (NN_DIGIT)bHigh * (NN_DIGIT)cLow;
+        a[1] = (NN_DIGIT)bHigh * (NN_DIGIT)cHigh;
+      
+        if ((t += u) < u) a[1] += TO_HIGH_HALF (1);
+        u = TO_HIGH_HALF (t);
+      
+        if ((a[0] += u) < u) a[1]++;
+        a[1] += HIGH_HALF (t);
+        
+        // Write outputs
+        out_data_low.write(a[0]);
+        out_data_high.write(a[1]);
+        break;
+
+    case S2_OUTPUT:
+        hw_mult_done.write(true);
+        break;
+
+    case S3_FINISH:
+        hw_mult_done.write(true);
+        break;
+  
+    default:
+        break;
+  }    
+}
+
+
+/*
 void dh_hw_mult::process_hw_mult()
 {
 	
@@ -13,33 +123,37 @@ void dh_hw_mult::process_hw_mult()
     if (hw_mult_enable.read() == true) 
     {	
 
-	// Read inputs	
-	b = in_data_1.read();
-	c = in_data_2.read();
-		
-	// Original code from NN_DigitMult()...		
-  	bHigh = (NN_HALF_DIGIT)HIGH_HALF (b);
-  	bLow = (NN_HALF_DIGIT)LOW_HALF (b);
-  	cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
-  	cLow = (NN_HALF_DIGIT)LOW_HALF (c);
+    	// Read inputs	
+    	b = in_data_1.read();
+    	c = in_data_2.read();
+    		
+    	// Original code from NN_DigitMult()...		
+    	bHigh = (NN_HALF_DIGIT)HIGH_HALF (b);
+    	bLow = (NN_HALF_DIGIT)LOW_HALF (b);
+    	cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
+    	cLow = (NN_HALF_DIGIT)LOW_HALF (c);
 
-  	a[0] = (NN_DIGIT)bLow * (NN_DIGIT)cLow;
-  	t = (NN_DIGIT)bLow * (NN_DIGIT)cHigh;
-  	u = (NN_DIGIT)bHigh * (NN_DIGIT)cLow;
-  	a[1] = (NN_DIGIT)bHigh * (NN_DIGIT)cHigh;
-  
-  	if ((t += u) < u) a[1] += TO_HIGH_HALF (1);
-  	u = TO_HIGH_HALF (t);
-  
-  	if ((a[0] += u) < u) a[1]++;
-  	a[1] += HIGH_HALF (t);
-		
- 	// Hardware multiplication delay = 100 ns
-	wait (100, SC_NS);
-	
-	// Write outputs
-	out_data_low.write(a[0]);
-	out_data_high.write(a[1]);
+    	a[0] = (NN_DIGIT)bLow * (NN_DIGIT)cLow;
+    	t = (NN_DIGIT)bLow * (NN_DIGIT)cHigh;
+    	u = (NN_DIGIT)bHigh * (NN_DIGIT)cLow;
+    	a[1] = (NN_DIGIT)bHigh * (NN_DIGIT)cHigh;
+    
+    	if ((t += u) < u) a[1] += TO_HIGH_HALF (1);
+    	u = TO_HIGH_HALF (t);
+    
+    	if ((a[0] += u) < u) a[1]++;
+    	a[1] += HIGH_HALF (t);
+  		
+     	// Hardware multiplication delay = 100 ns
+      // removed timed waits
+    	// wait (100, SC_NS);
+    	
+    	// Write outputs
+    	out_data_low.write(a[0]);
+    	out_data_high.write(a[1]);
+
+      // assert done signal
+      hw_mult_done.write(true);
 		
     }
 
@@ -48,3 +162,4 @@ void dh_hw_mult::process_hw_mult()
   }
 	  	  
 }
+*/
--- dh_hw_mult.h
+++ dh_hw_mult.h
@@ -4,6 +4,9 @@
 #ifndef _DH_HW_MULT_H_
 #define _DH_HW_MULT_H_ 1
 
+// enumerated types for states
+enum ctrl_state {S0_WAIT, S1_EXECUTE, S2_OUTPUT, S3_FINISH};
+
 SC_MODULE (dh_hw_mult)
 {
   sc_in<bool> hw_mult_enable; 
@@ -13,12 +16,37 @@ SC_MODULE (dh_hw_mult)
   sc_out<NN_DIGIT> out_data_high;
   sc_out<bool> hw_mult_done;
 
-  void process_hw_mult();
+  // void process_hw_mult();
+
+  //*************
+  // Added Ports
+  //*************
+  sc_in<sc_logic> reset; // reset port
+  sc_in_clk clock;   // clock input
+
+  //**************
+  // Added Signals
+  //**************
+  sc_signal<ctrl_state> state, next_state;  // state machine signals
+
+  //**************
+  // Added Functions
+  //**************
+  void state_transition();
+  void state_output();
+  void state_reg();
+
   
   SC_CTOR (dh_hw_mult)
   {
-    SC_THREAD (process_hw_mult);
-    sensitive << hw_mult_enable;
+    //SC_THREAD (process_hw_mult);
+    //sensitive << hw_mult_enable;
+
+    SC_METHOD(state_transition);
+      sensitive << state << hw_mult_enable;
+    SC_METHOD(state_output);
+      sensitive << state; 
+    SC_CTHREAD(state_reg, clock.pos())    // clock thread
   }
   
 };
